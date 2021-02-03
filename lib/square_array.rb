58721c8c2f1218b8c2747186ace95c7773eab0c8
Mathew E Newton <elijah2895@hotmail.com>
2021-02-03 21:31:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+# your code here
+def square_array(numbers)
+  array = [1,2,3]
+  new_numbers = []
+  numbers.length.times do |index|
+    new_numbers.push (array[index] * 2)
+  end
+  new_numbers
 end
